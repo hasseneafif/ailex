d181483f27913d616a7b087dfd0ed0a7d269d1ff
Hass <hasseneafif@gmail.com>
2025-09-13 12:31:06 +0200

    viewport

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -21,7 +21,9 @@ export default async function RootLayout({
 
   return (
     <html suppressHydrationWarning lang={locale} >
-  
+<head>
+  <meta name='viewport' content='width=device-width, initial-scale=1.0, viewport-fit=cover'></meta>
+  </head>  
 
       <body className="bg-gradient-to-br from-[#000505] via-black to-[#000404]">
         {/* <LenisSmoothScroll /> */}
--- components/Common/LenisSmoothScroll.tsx
+++ components/Common/LenisSmoothScroll.tsx
@@ -1,10 +1,10 @@
-"use client";
-import { useEffect } from "react";
-import { initSmoothScrolling } from "@/components/Common/smoothscroll";
+// "use client";
+// import { useEffect } from "react";
+// import { initSmoothScrolling } from "@/components/Common/smoothscroll";
 
-export default function LenisSmoothScroll() {
-  useEffect(() => {
-    initSmoothScrolling();
-  }, []);
-  return null;
-}
+// export default function LenisSmoothScroll() {
+//   useEffect(() => {
+//     initSmoothScrolling();
+//   }, []);
+//   return null;
+// }
